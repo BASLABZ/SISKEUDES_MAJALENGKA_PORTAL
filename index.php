036131cee7b3ab7184fa13f4e820ec7ef21f48db
SuperBase <tutorialsuper.cs@gmail.com>
2017-09-05 03:20:43 -0700

    MOVE TO GIT PUSH FIRST
    pindah ke git

---
+++ index.php
@@ -0,0 +1,291 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="assets/logo.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>SISKEUDES - Kab. Majalengka</title>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<!--     Fonts and icons     -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+	<!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-kit.css" rel="stylesheet"/>
+	<link href="assets/css/demo.css" rel="stylesheet" />
+
+</head>
+
+<body class="index-page">
+<!-- Navbar -->
+<nav class="navbar navbar-danger navbar-fixed-top">
+	<div class="container">
+        <div class="navbar-header">
+	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-index">
+	        	<span class="sr-only">Toggle navigation</span>
+	        	<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>
+	    	</button>
+	    	<a href="#">
+	        	<div class="logo-container">
+	                <div class="logo">
+	                    <img src="assets/logo.png" alt="Creative Tim Logo" rel="tooltip" title="<b>SISKEUDES</b> SIstem Informasi Keuangan Desa Kabupaten Majalengka <b>SISKEUDES</b>" data-placement="bottom" data-html="true">
+	                </div>
+	                <div class="brand" style="width: 200px;">
+	                    SISKEUDES Kabupaten Majalengka
+	                </div>
+
+
+				</div>
+	      	</a>
+	    </div>
+
+	    <div class="collapse navbar-collapse" id="navigation-index">
+	    	<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="#" target="_blank">
+						<i class="material-icons">book</i> Buku Panduan
+					</a>
+				</li>
+
+	    	</ul>
+	    </div>
+	</div>
+</nav>
+<!-- End Navbar -->
+
+<div class="wrapper">
+	<div class="header header-filter" style="background-image: url('assets/bg.jpg');">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="brand">
+						<h1 style="font-size: 24px;">SISKUDES KABUPATEN MAJALENGKA</h1>
+							<img src="assets/logo.png">
+						<h3>SISTEM INFORMASI KEUANGAN DESA KABUPATEN MAJALENGKA</h3>
+					
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="main main-raised">
+	    <div class="section section-notifications" id="notifications">
+	        <div class="alert alert-danger">
+	             <div class="container-fluid">
+					 <div class="alert-icon">
+						<i class="material-icons">error_outline</i>
+					</div>
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					
+					</button>
+	                 <b>SISKEUDES Meliputi: Sistem Informasi Pengelolahan Perencanaan , Penganggaran & Penatausahaan Desa</b>
+	            </div>
+	        </div>
+	       <div class="container">
+				<div class="row">
+					<div class="col-md-4">
+					
+						<div class="card card-nav-tabs" style="background-color: #0ab1fc;">
+							<div class="header header-success" style="background: linear-gradient(60deg, #66bb6a, #09adf7);">
+							
+								<div class="nav-tabs-navigation">
+									<div class="nav-tabs-wrapper">
+										<ul class="nav nav-tabs" data-tabs="tabs">
+											<li class="active">
+												<a href="#profile" data-toggle="tab" style="font-size: 10px;">
+													<i class="material-icons">home</i>
+													PERENCANAAN DESA
+												</a>
+											</li>
+											<li>
+												<a href="#profile" data-toggle="tab" style="font-size: 10px;">
+												<i class="material-icons">link</i>
+													Panduan
+												</a>
+											</li>
+											
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="content">
+								<div class="tab-content text-center">
+									<div class="tab-pane active" id="profile">
+										<p style="color: white;"> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.  </p>
+										<a href="#" class="btn btn-upgrade btn-lg" target="_blank">
+							<i class="material-icons">link</i> MASUK 
+						<div class="ripple-container"></div></a>
+
+
+									</div>
+									
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div class="col-md-4">
+						<div class="card card-nav-tabs" style="background-color: #0ab1fc;" >
+							<div class="header header-success" style="background: linear-gradient(60deg, #66bb6a, #09adf7);">
+								<div class="nav-tabs-navigation">
+									<div class="nav-tabs-wrapper">
+										<ul class="nav nav-tabs" data-tabs="tabs">
+											<li class="active">
+												<a href="#profile" data-toggle="tab" style="font-size: 10px;">
+													<i class="material-icons">home</i>
+													PENGANGGARAN DESA
+												</a>
+											</li>
+											<li>
+												<a href="#profile" data-toggle="tab" style="font-size: 10px;">
+												<i class="material-icons">link</i>
+													Panduan
+												</a>
+											</li>
+
+											
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="content">
+								<div class="tab-content text-center">
+									<div class="tab-pane active" id="profile">
+										<p style="color: white;"> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. </p>
+										<a href="http://localhost/apbdes_2015/index.apbdes" class="btn btn-upgrade btn-lg" target="_blank">
+							<i class="material-icons">link</i> MASUK 
+						<div class="ripple-container"></div></a>
+
+
+									</div>
+									
+								</div>
+							</div>
+						</div>
+						
+
+					</div>
+					<div class="col-md-4">
+						<div class="card card-nav-tabs" style="background-color: #0ab1fc;">
+							<div class="header header-success" style="background: linear-gradient(60deg, #66bb6a, #09adf7);">
+							
+								<div class="nav-tabs-navigation">
+									<div class="nav-tabs-wrapper">
+										<ul class="nav nav-tabs" data-tabs="tabs">
+											<li class="active">
+												<a href="#profile" data-toggle="tab" style="font-size: 10px;">
+													<i class="material-icons">home</i>
+													PENATAUSAHAAN DESA
+												</a>
+											</li>
+											<li>
+												<a href="#profile" data-toggle="tab" style="font-size: 10px;">
+												<i class="material-icons">link</i>
+													Panduan
+												</a>
+											</li>
+											
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="content">
+								<div class="tab-content text-center">
+									<div class="tab-pane active" id="profile">
+										<p style="color: white;"> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. </p>
+										<a href="http://localhost/apbdes_2017/auth" class="btn btn-upgrade btn-lg" target="_blank">
+							<i class="material-icons">link</i> MASUK 
+						<div class="ripple-container"></div></a>
+
+
+									</div>
+									
+								</div>
+							</div>
+						</div>
+				
+
+					</div>
+				</div>
+			</div>
+	    </div>
+			<div class="clearfix" style="padding-bottom: 40px;"></div>
+
+	</div>
+    <footer class="footer">
+	    <div class="container">
+	        <nav class="pull-left">
+	            <ul>
+					<li>
+						<a href="http://www.majalengkakab.go.id">
+							DPMD Kab. Majalengka
+						</a>
+					</li>
+					<li>
+						<a href="http://www.majalengkakab.go.id">
+						   kabupaten Majalengka</a>
+					</li>
+	            </ul>
+	        </nav>
+	        <div class="copyright pull-right">
+	            &copy; <?php echo date('Y') ?>, by PT. Global Intermedia.
+	        </div>
+	    </div>
+	</footer>
+</div>
+
+<!-- Sart Modal -->
+<!-- <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+					<i class="material-icons">clear</i>
+				</button>
+				<h4 class="modal-title">Modal title</h4>
+			</div>
+			<div class="modal-body">
+				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+				</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default btn-simple">Nice Button</button>
+				<button type="button" class="btn btn-danger btn-simple" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div> -->
+<!--  End Modal -->
+
+
+</body>
+	<!--   Core JS Files   -->
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/js/material.min.js"></script>
+	<script src="assets/js/nouislider.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+	<script src="assets/js/material-kit.js" type="text/javascript"></script>
+
+	<script type="text/javascript">
+
+		$().ready(function(){
+			// the body of this function is in assets/material-kit.js
+			materialKit.initSliders();
+            window_width = $(window).width();
+
+            if (window_width >= 992){
+                big_image = $('.wrapper > .header');
+
+				$(window).on('scroll', materialKitDemo.checkScrollForParallax);
+			}
+
+		});
+	</script>
+</html>
